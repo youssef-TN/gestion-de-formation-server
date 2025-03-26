df89b3e0b17651f3f5e96d62a700c7ddb52e4bb3
youssef-TN <youssefbenrhaiem.tn@gmail.com>
2025-03-26 13:29:52 +0100

    1st commit

---
+++ src/main/java/PO_BD/gestion_de_formation/GestionDeFormationApplication.java
@@ -0,0 +1,13 @@
+package PO_BD.gestion_de_formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeFormationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PO_BD/gestion_de_formation/GestionDeFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package PO_BD.gestion_de_formation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeFormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
